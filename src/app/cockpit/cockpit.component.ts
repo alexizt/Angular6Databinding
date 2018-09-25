44bb06b4689ea57a9bebb870d10173581d766208
atriemstra <alexis.triemstra@corcoran.com>
2018-09-25 12:21:25 -0300

    Initial Commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  newServerName = '';
+  newServerContent = '';
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // bpCreated will be the name exposed to outside
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName:string, serverContent: string }>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+}
