fc54df61b2a5e33ec209f14751acba2f77ff45c2
Alexis <atriemstra@ARG-DMJGEFXY.corp.global.level3.com>
2018-09-28 12:31:04 -0300

    Updated Angular Cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Element } from './element.model';
 
 @Component({
@@ -6,9 +6,15 @@ import { Element } from './element.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   //serverElements = [{type: 'server', name: 'TestServer!', content: 'Just a test'}];
   serverElements = [ new Element('server', 'TestServer!', 'Just a test') ];
+
+  ngOnInit() {
+    this.serverElements.filter((element, index, array)=>{
+        return element.name.length>0;
+    })
+  }
   
   onServerAdded(serverData: {serverName: string, serverContent: string}) {
     this.serverElements.push(
